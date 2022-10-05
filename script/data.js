c6ca21d111eed2a8812a67f31339565090ceb345
lourenzokodama <kodamalourenzo@gmail.com>
2022-10-04 22:48:02 -0600

    Update data.js

--- script/data.js
+++ script/data.js
@@ -1,3 +1,3 @@
 const d = new Date();
-document.getElementById("d").innerHTML = d.getFullYear();
+document.getElementById("year").innerHTML = d.getFullYear();
 document.getElementById("time").innerHTML = "Last updated:"+document.lastModified
