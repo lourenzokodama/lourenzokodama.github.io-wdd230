9b80cce9d656c9ae5d67cba5f94b6a301dbce622
lourenzokodama <kodamalourenzo@gmail.com>
2022-10-08 15:58:27 -0600

    Update data.js

--- chamber/script/data.js
+++ chamber/script/data.js
@@ -4,21 +4,5 @@ document.getElementById("year").innerHTML = d.getFullYear();
 document.getElementById("time").innerHTML = "Last updated:"+document.lastModified
 
 
-function toggleMenu() {
-    var temp = document.getElementsByClassName("toggler")[0]
-    temp.classList.toggle("hide")
-    if (temp.classList[1]=="hide") {
-        document.querySelector(".toggler > a").innerHTML = "&#9776; Menu"
-    }
-    else {
-        document.querySelector(".toggler > a").innerHTML = "&#10006; Close"
-    }
-}
 
-if (new Date().getDay() == 5) {
-    document.getElementById("big-banner").style.display = "grid"
-}
 
-function closeBanner() {
-    document.getElementById("big-banner").style.display = "none"
-}
