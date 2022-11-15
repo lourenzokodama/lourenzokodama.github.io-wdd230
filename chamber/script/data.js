4f14440ccca0dae1e965a8c25c4ae8c433d29677
lourenzokodama <kodamalourenzo@gmail.com>
2022-11-15 14:40:00 -0700

    Update data.js

--- chamber/script/data.js
+++ chamber/script/data.js
@@ -1,4 +1,30 @@
-const d = new Date();
-document.getElementById("year").innerHTML = d.getFullYear();
-document.getElementById("time").innerHTML = "Last updated:"+document.lastModified
+function toggleMenu(){
+    
+    document.getElementById("time").classList.toggle("open");
+    document.getElementById("year").classList.toggle("open");
+}
 
+const ham = document.getElementById("hamburgerBtn");
+
+ham.onclick = toggleMenu;
+
+// footer date
+const date = new Date(document.lastModified);
+document.getElementById("updateDate").innerHTML = document.lastModified;
+
+
+// header date
+const today = new Date();
+const longDay = new Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(today);
+const headerDate = document.querySelector(".headerDate");
+headerDate.innerHTML = `${longDay}`;
+
+/*join button or banner to display certain days of week*/
+function toggleBanner(){
+    /*document.getElementById("banner").classList.toggle("show");*/
+    document.getElementById("flash").classList.toggle("show");
+}   
+const dow = today.getDay();
+if (dow == 1 || dow == 2) {
+  
+    toggleBanner();
